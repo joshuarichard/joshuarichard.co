ec106cb00944a8259f29b0a2e4d09e289b7be857
joshuarichard <eos.josh.richard@gmail.com>
2021-06-30 20:06:27 -0400

    euo pipefall

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefall
+
 NODE_VERSION='12.14.1'
 export NVM_DIR="$HOME/.nvm"
 source "$NVM_DIR/nvm.sh"
@@ -7,6 +9,8 @@ nvm use --silent $NODE_VERSION
 NODE_PATH=$(which node)
 export NODE_PATH=$NODE_PATH
 
+yarn
+
 yarn build
 
-cp build/ /var/www/html/
+cp -r build/ /var/www/html/
